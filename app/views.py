52dc85c6ec91aad2d3fc32f96f10ac465aa0ed2f
BradOdera <bradodera@gmail.com>
2019-11-19 12:24:53 +0300

    generate views for the app

--- app/views.py
+++ app/views.py
@@ -1,19 +1,35 @@
 from flask import render_template
 from app import app
+from .request import get_sources,get_articles
 
-
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html', id=movie_id)
-
-
+# views
+@app.route('/')
 def index():
     '''
-    View root page function that returns the index page and its data
+    root page that returns the index page
     '''
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title=title)
+    # Getting news sources
+    sources = get_sources()
+    
+    #getting articles 
+    # articles = get_articles()
+
+
+    return render_template('index.html', source = sources)
+
+
+@app.route('/sources/<id>')
+def sources(id):
+    '''
+    news page function that returns news details page
+    '''
+    sources = get_sources()
+    heading = None
+    for source in sources:
+        if id == source.id:
+            heading = source.name
+
+    articles = get_articles(id)
+
+    return render_template('articles.html',article = articles , heading  = heading)
