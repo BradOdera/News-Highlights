4f5717cbbf7559db7d699419ea02f1ba41ab626f
BradOdera <bradodera@gmail.com>
2019-11-19 12:18:07 +0300

    import devconfig from config

--- app/_init_.py
+++ app/_init_.py
@@ -1,14 +1,9 @@
-# from app import views
-# from flask import Flask
-
-# app = Flask(__name__)
-
 from app import views
 from flask import Flask
 from .config import DevConfig
 
-# Initializing application
+
 app = Flask(__name__)
 
-# Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
