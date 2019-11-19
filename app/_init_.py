aa4d80965b50599d7d12b242ee9ab23234738173
BradOdera <bradodera@gmail.com>
2019-11-19 09:58:45 +0300

    Add api key

--- app/_init_.py
+++ app/_init_.py
@@ -1,4 +1,14 @@
+# from app import views
+# from flask import Flask
+
+# app = Flask(__name__)
+
 from app import views
 from flask import Flask
+from .config import DevConfig
 
+# Initializing application
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
--- app/views.py
+++ app/views.py
@@ -2,9 +2,18 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html', id=movie_id)
+
+
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title=title)
