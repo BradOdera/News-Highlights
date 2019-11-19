343aceb5ba679794a747da989b30e8feda5f294f
BradOdera <bradodera@gmail.com>
2019-11-19 13:23:54 +0300

    Try to fix error

--- app/_init_.py
+++ app/_init_.py
@@ -1,4 +1,3 @@
-from app import views
 from flask import Flask
 from .config import DevConfig
 
@@ -7,3 +6,5 @@ app = Flask(__name__)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+from app import views
